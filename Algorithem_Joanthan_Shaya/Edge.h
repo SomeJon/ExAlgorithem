d8bd182133b6439dab3691914eaddfdda213196f
Jonathan Shaya <jonathansh2@mta.ac.il>
2024-05-29 23:16:43 +0300

    added members for edge and vertex

--- Algorithem_Joanthan_Shaya/Edge.h
+++ Algorithem_Joanthan_Shaya/Edge.h
@@ -4,4 +4,21 @@
 #include "Base.h"
 #include "Vertex.h"
 
+class Edge {
+private:
+	Vertex& m_Left;
+	Vertex& m_Right;
+
+public:
+	Vertex& getLeft()				{ return m_Left;	}
+	void setLeft(Vertex& i_Left)	{ m_Left = i_Left;	}
+	Vertex& getRight()				{ return m_Right;	}
+	void setRight(Vertex& i_Right)	{ m_Right = i_Right; }
+
+	Edge(Vertex& i_Left, Vertex& i_Right){
+		setLeft(i_Left);
+		setRight(i_Right);
+	}
+};
+
 #endif
--- Algorithem_Joanthan_Shaya/Vertex.h
+++ Algorithem_Joanthan_Shaya/Vertex.h
@@ -4,5 +4,26 @@
 #include "Base.h"
 #include "Edge.h"
 
+class Vertex {
+private:
+	int m_Point;
+	std::forward_list<Edge> m_Adjacent;
+
+public:
+	int getPoint()									{ return m_Point;		}
+	void setPoint(int i_PointId)					{ m_Point = i_PointId;	}
+	std::forward_list<Edge> getAdjList()			{ return m_Adjacent;	}
+	void setAdjList(std::forward_list<Edge> i_List) { m_Adjacent = i_List;	}
+	Edge addEdge(Vertex i_Adjacent) {
+		Edge toAdd(*this, i_Adjacent);
+		m_Adjacent.push_front(toAdd);
+		return toAdd;
+	}
+
+	Vertex(int i_PointId) {
+		setPoint(i_PointId);
+		m_Adjacent = std::forward_list<Edge>();
+	}
+};
 
 #endif
